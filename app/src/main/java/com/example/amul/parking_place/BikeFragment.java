905944ae4efdb9f96239190f126b413b5754a801
amul <amalan.solomonraj@gmail.com>
2019-05-16 12:54:22 +0530

    first commit

---
+++ app/src/main/java/com/example/amul/parking_place/BikeFragment.java
@@ -0,0 +1,17 @@
+package com.example.amul.parking_place;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BikeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bike,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/amul/parking_place/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.amul.parking_place;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new CarFragment()).commit();
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_car:
+                    selectedFragment = new CarFragment();
+                    break;
+                case R.id.nav_bike:
+                    selectedFragment = new BikeFragment();
+                    break;
+                case R.id.nav_parking:
+                    selectedFragment = new ParkingFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+}
